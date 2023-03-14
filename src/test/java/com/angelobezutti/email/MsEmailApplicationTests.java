8b48db131c001d2cf61df9dc013e6ea039e808d6
Angelo Bezutti <angeloantoniobzt@gmail.com>
2023-03-14 16:14:38 -0300

    Init

---
+++ src/test/java/com/angelobezutti/email/MsEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelobezutti.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
